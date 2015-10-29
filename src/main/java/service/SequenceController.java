1fb835fb9ae848eb06732e43ae23cc78cf1ec82d
Taylor Peer <t.peer@cortical.io>
2015-10-29 09:42:18 +0100

    Added quality check of generated sequences

--- src/main/java/service/SequenceController.java
+++ src/main/java/service/SequenceController.java
@@ -8,11 +8,14 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +33,8 @@ public class SequenceController {
 
 	private HTMSequenceGenerator generator;
 
+	private String lastSequenceReturned = "";
+
 	@RequestMapping("/learn")
 	public void learn(@RequestParam(value = "sequence", required = true) String sequenceString,
 			@RequestParam(value = "bpm") float bpm, @RequestParam(value = "memoryBank") int memoryBank) {
@@ -39,7 +44,7 @@ public class SequenceController {
 		sb.append("user_sequence" + "\t");
 		sb.append(bpm + "\t");
 		sb.append(sequenceString);
-		
+
 		try {
 			Files.write(Paths.get(TRAINING_FILE), ("\n" + sb.toString()).getBytes(), StandardOpenOption.APPEND);
 		} catch (IOException e) {
@@ -59,66 +64,86 @@ public class SequenceController {
 			return new Sequence("0000000000000000", bpm);
 		}
 
-		// Reset the HTM on every request
-		generator = new HTMSequenceGenerator();
+		String sequenceString = new String();
+		boolean validSequence = false;
+		boolean seedSequenceWasSet = (seedSequenceString != null);
+		while (!validSequence) {
 
-		List<String> trainingSubset = new ArrayList<String>();
-		Map<Sequence, Float> sequenceScores = new HashMap<Sequence, Float>();
+			// Reset the HTM
+			generator = new HTMSequenceGenerator();
 
-		// Score training sequences by request parameters
-		if (seedSequenceString != null) {
-			// If a seed sequence was received, order all training data by
-			// similarity to it
+			List<String> trainingSubset = new ArrayList<String>();
+			Map<Sequence, Float> sequenceScores = new HashMap<Sequence, Float>();
 			Sequence seedSequence = new Sequence(seedSequenceString, bpm);
+
+			// Select a seed sequence if none was specified
+			if (!seedSequenceWasSet) {
+				Random randomizer = new Random();
+				int randomIndex = randomizer.nextInt(trainingSequences.size());
+				seedSequence = trainingSequences.get(randomIndex);
+				seedSequenceString = seedSequence.getSequence();
+			}
+
 			trainingSubset.add(seedSequenceString);
+			trainingSequences.remove(seedSequenceString);
+			
+			System.out.println("Using  " + seedSequenceString + " as seed.");
+			
+			// Score all other sequences by their similarity to the selected seed
 			for (Sequence sequence : trainingSequences) {
 				sequenceScores.put(sequence, seedSequence.getSimilarity(sequence));
 			}
-		} else {
 
-			// Order all training data by similarity score to received BPM
-			for (Sequence sequence : trainingSequences) {
-				float bpmScore = Math.abs(bpm - sequence.getBpm());
-				sequenceScores.put(sequence, bpmScore);
-			}
-
-			// Select a seed sequence
+			// Select top scoring training sequences
 			sequenceScores = MapUtilities.sortByValue(sequenceScores);
-			Sequence seedSequence = null;
 			for (Sequence key : sequenceScores.keySet()) {
-				seedSequence = key;
-				break;
+				int random = ThreadLocalRandom.current().nextInt(0, 10 + 1);
+				if (trainingSubset.size() < 2 && random > 6 && !seedSequenceString.equalsIgnoreCase(key.getSequence())) {
+					System.out.println(
+							"Adding " + key.getSequence() + " as training sequence. Score: " + sequenceScores.get(key));
+					trainingSubset.add(key.getSequence());
+				}
 			}
-			trainingSubset.add(seedSequence.getSequence());
-			// Order all training data by similarity to selected seed
-			sequenceScores.clear();
-			for (Sequence sequence : trainingSequences) {
-				sequenceScores.put(sequence, seedSequence.getSimilarity(sequence));
+
+			// TODO ensure training subset is correct size
+
+			// Select first hit at random from trainingSubset
+			int random = ThreadLocalRandom.current().nextInt(0, trainingSubset.size());
+			Double firstHit = Double.valueOf(trainingSubset.get(random).substring(0, 1));
+
+			// Train HTM, create and return new sequence
+			sequenceString = generator.generate(trainingSubset, firstHit);
+
+			// Ensure the sequence is not too sparse or uniform
+			int emptyBeats = StringUtils.countMatches(sequenceString, "0");
+			List<String> digitsSeen = new ArrayList<String>();
+			for (String digit : new String[] { "0", "1", "2", "3", "4", "5", "6", "7" }) {
+				int count = StringUtils.countMatches(sequenceString, digit);
+				if (count > 1) {
+					digitsSeen.add(digit);
+				}
+			}
+			int sequenceSparsityThreshold = (sequenceString.length() / 2) + 1;
+			if (emptyBeats <= sequenceSparsityThreshold && digitsSeen.size() > 2
+					&& !lastSequenceReturned.equalsIgnoreCase(sequenceString)) {
+				validSequence = true;
+			} else {
+				if (emptyBeats > sequenceSparsityThreshold) {
+					System.out.println("Sequence was too sparse: " + sequenceString + " (contained " + emptyBeats
+							+ " empty beats)");
+				} else if (digitsSeen.size() < 2) {
+					System.out.println("Sequence did not contain enough drum hits: " + sequenceString + " (contained "
+							+ digitsSeen.size() + " unique drums)");
+				} else if (lastSequenceReturned.equalsIgnoreCase(sequenceString)) {
+					System.out.println("Sequence was equal to the last generated sequence: " + sequenceString);
+				}
+
 			}
 		}
 
-		// Select top scoring training sequences
-		sequenceScores = MapUtilities.sortByValue(sequenceScores);
-		for (Sequence key : sequenceScores.keySet()) {
-			int random = ThreadLocalRandom.current().nextInt(0, 10 + 1);
-			if (trainingSubset.size() < 4 && random > 3) {
-				System.out.println(
-						"Adding " + key.getSequence() + " as training sequence. Score: " + sequenceScores.get(key));
-				trainingSubset.add(key.getSequence());
-			}
-		}
+		lastSequenceReturned = sequenceString;
 
-		// TODO ensure training subset is correct size
-		System.out.println(trainingSubset.size());
-
-		// Select first hit at random from trainingSubset
-		int random = ThreadLocalRandom.current().nextInt(0, trainingSubset.size());
-		Double firstHit = Double.valueOf(trainingSubset.get(random).substring(0, 1));
-
-		// Train HTM, create and return new sequence
-		String sequence = generator.generate(trainingSubset, firstHit);
-
-		return new Sequence(sequence, bpm);
+		return new Sequence(sequenceString, bpm);
 	}
 
 	private static List<Sequence> loadTrainingData(int bank) {
@@ -150,6 +175,10 @@ public class SequenceController {
 				}
 			}
 		}
+
+		// Shuffle the list to ensure a random ordering
+		long seed = System.nanoTime();
+		Collections.shuffle(sequences, new Random(seed));
 
 		return sequences;
 	}
