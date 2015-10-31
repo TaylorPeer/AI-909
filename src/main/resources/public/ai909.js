4017027f51f94bfcc138c7462deba08a72b23eaa
Taylor Peer <t.peer@cortical.io>
2015-10-31 11:51:34 +0100

    iOS compatibility stuff

--- src/main/resources/public/ai909.js
+++ src/main/resources/public/ai909.js
@@ -12,13 +12,58 @@ Sequencer.sequenceString = "0000000000000000";
 Sequencer.bank = 1;
 
 var waitingForResponse = false;
+var interactionEvent = "click";
+
+function rebindMobileEvents() {
+
+    var $kick = $("#kick");
+    $kick.unbind("click");
+    $kick.bind(interactionEvent, function () {
+        createjs.Sound.play("kick");
+    });
+
+    var $snare = $("#snare");
+    $snare.unbind("click");
+    $snare.bind(interactionEvent, function () {
+        createjs.Sound.play("snare");
+    });
+
+    var $hihat = $("#hihat");
+    $hihat.unbind("click");
+    $hihat.bind(interactionEvent, function () {
+        createjs.Sound.play("hihat");
+    });
+
+    var $play = $("#play");
+    $play.unbind("click");
+    $play.bind(interactionEvent, function () {
+        Sequencer.playing = !Sequencer.playing;
+        var $playIcon = $("#play-icon");
+        if (Sequencer.playing) {
+            play();
+            $playIcon.removeClass("glyphicon-play");
+            $playIcon.addClass("glyphicon-pause");
+        } else {
+            $playIcon.removeClass("glyphicon-pause");
+            $playIcon.addClass("glyphicon-play");
+        }
+    });
+}
 
 $(document).ready(function () {
+
+    // device detection
+    var isMobile = false;
+    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) isMobile = true;
+
+    if (isMobile) {
+        interactionEvent = "touchstart";
+    }
 
     loadSounds();
 
     // Add listeners to all step buttons
-    $(".drum-button").not(".drum-label").click(function () {
+    $(".drum-button").not(".drum-label").bind(interactionEvent, function () {
 
         if (!waitingForResponse) {
 
@@ -36,19 +81,29 @@ $(document).ready(function () {
         }
     });
 
-    $("#kick").click(function () {
+
+    $("#kick").bind("click", function () {
         createjs.Sound.play("kick");
+        if (interactionEvent != "click") {
+            rebindMobileEvents();
+        }
     });
 
-    $("#snare").click(function () {
+    $("#snare").bind("click", function () {
         createjs.Sound.play("snare");
+        if (interactionEvent != "click") {
+            rebindMobileEvents();
+        }
     });
 
-    $("#hihat").click(function () {
+    $("#hihat").bind("click", function () {
         createjs.Sound.play("hihat");
+        if (interactionEvent != "click") {
+            rebindMobileEvents();
+        }
     });
 
-    $("#play").click(function () {
+    $("#play").bind("click", function () {
         Sequencer.playing = !Sequencer.playing;
         var $playIcon = $("#play-icon");
         if (Sequencer.playing) {
@@ -59,9 +114,12 @@ $(document).ready(function () {
             $playIcon.removeClass("glyphicon-pause");
             $playIcon.addClass("glyphicon-play");
         }
+        if (interactionEvent != "click") {
+            rebindMobileEvents();
+        }
     });
 
-    $("#stop").click(function () {
+    $("#stop").bind(interactionEvent, function () {
         Sequencer.playing = false;
         Sequencer.step = 0;
         $(".step-indicator").fadeOut(50);
@@ -70,7 +128,7 @@ $(document).ready(function () {
         $playIcon.addClass("glyphicon-play");
     });
 
-    $("#generate").click(function () {
+    $("#generate").bind(interactionEvent, function () {
         if (!waitingForResponse) {
             waitingForResponse = true;
             blink();
@@ -81,7 +139,7 @@ $(document).ready(function () {
         }
     });
 
-    $("#mutate").click(function () {
+    $("#mutate").bind(interactionEvent, function () {
         if (!waitingForResponse) {
             waitingForResponse = true;
             blink();
@@ -92,35 +150,35 @@ $(document).ready(function () {
         }
     });
 
-    $("#learn").click(function () {
+    $("#learn").bind(interactionEvent, function () {
         $.get("/learn?sequence=" + getSeedString() + "&bpm=" + Sequencer.bpm + "&memoryBank=" + Sequencer.bank);
     });
 
-    $("#lower-bpm").click(function () {
+    $("#lower-bpm").bind(interactionEvent, function () {
         Sequencer.bpm = Math.max(Sequencer.bpm - 1, 1);
         updateBpmDisplay();
     });
 
-    $("#increase-bpm").click(function () {
+    $("#increase-bpm").bind(interactionEvent, function () {
         Sequencer.bpm = Math.min(Sequencer.bpm + 1, 999);
         updateBpmDisplay();
     });
 
-    $("#lower-bank").click(function () {
+    $("#lower-bank").bind(interactionEvent, function () {
         Sequencer.bank = Math.max(Sequencer.bank - 1, 1);
         updateBankDisplay();
     });
 
-    $("#increase-bank").click(function () {
+    $("#increase-bank").bind(interactionEvent, function () {
         Sequencer.bank = Math.min(Sequencer.bank + 1, 99);
         updateBankDisplay();
     });
 });
 
 function loadSounds() {
-    createjs.Sound.registerSound("audio/kick 01.wav", "kick");
-    createjs.Sound.registerSound("audio/snare 14.wav", "snare");
-    createjs.Sound.registerSound("audio/hh 03.wav", "hihat");
+    createjs.Sound.registerSound("audio/kick 01.mp3", "kick");
+    createjs.Sound.registerSound("audio/snare 14.mp3", "snare");
+    createjs.Sound.registerSound("audio/hh 03.mp3", "hihat");
 }
 
 function play() {
@@ -143,13 +201,12 @@ function play() {
     }
     $(stepIndicator[previousStep]).fadeOut(50);
 
-
     Sequencer.step = Sequencer.step + 1;
     if (Sequencer.step > 15) {
         Sequencer.step = 0;
     }
 
-    var delay = 60000 / Sequencer.bpm / 2;
+    var delay = 60000 / Sequencer.bpm / 2 / 2; // TODO temp?
     setTimeout(function () {
         if (Sequencer.playing) {
             play();
@@ -298,343 +355,4 @@ function blink() {
             }
         }, delay);
     }, delay);
-
-}
-
-if (!window.console) console = {
-    log: function () {
-    }
-};
-
-var lowLag = new function () {
-    this.someVariable = undefined;
-    this.showNeedInit = function () {
-        lowLag.msg("lowLag: you must call lowLag.init() first!");
-    }
-
-    this.load = this.showNeedInit;
-    this.play = this.showNeedInit;
-
-    this.useSuspension = false;
-    this.suspendDelay = 10000; // ten seconds
-    this.suspendTimeout = null;
-    this.suspended = false;
-
-    this.audioTagTimeToLive = 5000;
-
-    this.sm2url = 'sm2/swf/';
-
-    this.soundUrl = "";
-
-    this.debug = "console";
-
-    this.divLowLag = null;
-    this.divDebug = null;
-
-    this.createElement = function (elemType, attribs) {
-        var elem = document.createElement(elemType);
-        if (attribs) {
-            for (var key in attribs) {
-                elem.setAttribute(key, attribs[key]);
-            }
-        }
-        return elem;
-    };
-    this.safelyRemoveElement = function (elem) {
-        if (elem) elem.parentNode.removeChild(elem);
-    };
-    this.safelyRemoveElementById = function (id) {
-        this.safelyRemoveElement(document.getElementById(id));
-    };
-
-    this.ready = function ready(fn) {
-        if (document.readyState != 'loading') {
-            fn();
-        } else if (document.addEventListener) {
-            document.addEventListener('DOMContentLoaded', fn);
-        } else {
-            document.attachEvent('onreadystatechange', function () {
-                if (document.readyState != 'loading')
-                    fn();
-            });
-        }
-    };
-
-    this.init = function (config) {
-        //var divLowLag = document.getElementById("lowLag");
-        this.safelyRemoveElement(this.divLowLag);
-        this.divLowLag = this.createElement("div", {"id": "lowLag"});
-        document.body.appendChild(this.divLowLag);
-
-        var force = undefined;
-        if (config != undefined) {
-            if (config['force'] != undefined) {
-                force = config['force'];
-            }
-            if (config['audioTagTimeToLive'] != undefined) {
-                lowLag.audioTagTimeToLive = config['audioTagTimeToLive'];
-            }
-            if (config['sm2url'] != undefined) {
-                lowLag.sm2url = config['sm2url'];
-            }
-            if (config['urlPrefix'] != undefined) {
-                lowLag.soundUrl = config['urlPrefix'];
-            }
-            if (config['debug'] != undefined) {
-                lowLag.debug = config['debug'];
-            }
-            if (config['useSuspension'] != undefined) {
-                lowLag.useSuspension = config['useSuspension'];
-            }
-            if (config['suspendDelay'] != undefined) {
-                lowLag.suspendDelay = config['suspendDelay'];
-            }
-        }
-
-        if (lowLag.debug == "screen" || lowLag.debug == "both") {
-            lowLag.divDebug = lowLag.createElement("pre");
-            lowLag.divLowLag.appendChild(lowLag.divDebug);
-
-        }
-
-        var format = "sm2";
-        if (force != undefined) format = force; else {
-            if (typeof(webkitAudioContext) != "undefined") format = 'webkitAudio'; else if (navigator.userAgent.indexOf("Firefox") != -1) format = 'audioTag';
-        }
-        switch (format) {
-            case 'webkitAudio':
-
-                this.msg("init webkitAudio");
-                this.load = this.loadSoundWebkitAudio;
-                this.play = this.playSoundWebkitAudio;
-                this.webkitAudioContext = new webkitAudioContext();
-                if (this.useSuspension &= ('suspend' in lowLag.webkitAudioContext && 'onended' in lowLag.webkitAudioContext.createBufferSource())) {
-                    this.playingQueue = [];
-                    this.suspendPlaybackWebkitAudio();
-                }
-                break;
-            case 'audioTag':
-                this.msg("init audioTag");
-                this.load = this.loadSoundAudioTag;
-                this.play = this.playSoundAudioTag;
-                break;
-
-            case 'sm2':
-                this.msg("init SoundManager2");
-
-                this.load = this.loadSoundSM2;
-                this.play = this.playSoundSM2;
-                lowLag.msg("loading SM2 from " + lowLag.sm2url);
-                soundManager.setup({
-                    url: lowLag.sm2url, useHighPerformance: true, onready: lowLag.sm2Ready, debugMode: true
-                })
-
-                break;
-
-        }
-
-    }
-    this.sm2IsReady = false;
-    //sm2 has a callback that tells us when it's ready, so we may need to store
-    //requests to loadsound, and then call sm2 once it has told us it is set.
-    this.sm2ToLoad = [];
-
-    this.loadSoundSM2 = function (url, tag) {
-        if (lowLag.sm2IsReady) {
-            lowLag.loadSoundSM2ForReals(url, tag);
-        } else {
-            lowLag.sm2ToLoad.push([url, tag]);
-        }
-    }
-
-    this.loadSoundSM2ForReals = function (urls, ptag) {
-        var tag = lowLag.getTagFromURL(urls, ptag);
-        lowLag.msg('sm2 loading ' + urls + ' as tag ' + tag);
-        var urls = lowLag.getURLArray(urls); //coerce
-        for (var i = 0; i < urls.length; i++) {
-            var url = lowLag.soundUrl + urls[i];
-            urls[i] = url;
-        }
-
-        soundManager.createSound({
-            id: tag, autoLoad: true, url: urls
-        });
-    };
-
-    this.sm2Ready = function () {
-        lowLag.sm2IsReady = true;
-        for (var i = 0; i < lowLag.sm2ToLoad.length; i++) {
-            var urlAndTag = lowLag.sm2ToLoad[i];
-            lowLag.loadSoundSM2ForReals(urlAndTag[0], urlAndTag[1]);
-        }
-        lowLag.sm2ToLoad = [];
-    }
-
-    this.playSoundSM2 = function (tag) {
-        lowLag.msg("playSoundSM2 " + tag);
-
-        soundManager.play(tag);
-    }
-
-    //we'll use the tag they hand us, or else the url as the tag if it's a single tag,
-    //or the first url
-    this.getTagFromURL = function (url, tag) {
-        if (tag != undefined) return tag;
-        return lowLag.getSingleURL(url);
-    }
-    this.getSingleURL = function (urls) {
-        if (typeof(urls) == "string") return urls;
-        return urls[0];
-    }
-    //coerce to be an array
-    this.getURLArray = function (urls) {
-        if (typeof(urls) == "string") return [urls];
-        return urls;
-    }
-
-    this.webkitPendingRequest = {};
-
-    this.webkitAudioContext = undefined;
-    this.webkitAudioBuffers = {};
-
-    this.loadSoundWebkitAudio = function (urls, tag) {
-        var url = lowLag.getSingleURL(urls);
-        var tag = lowLag.getTagFromURL(urls, tag);
-        lowLag.msg('webkitAudio loading ' + url + ' as tag ' + tag);
-        var request = new XMLHttpRequest();
-        request.open('GET', lowLag.soundUrl + url, true);
-        request.responseType = 'arraybuffer';
-
-        // Decode asynchronously
-        request.onload = function () {
-            lowLag.webkitAudioContext.decodeAudioData(request.response, function (buffer) {
-                lowLag.webkitAudioBuffers[tag] = buffer;
-
-                if (lowLag.webkitPendingRequest[tag]) { //a request might have come in, try playing it now
-                    lowLag.playSoundWebkitAudio(tag);
-                }
-            }, lowLag.errorLoadWebkitAudtioFile);
-        };
-        request.send();
-    }
-
-    this.errorLoadWebkitAudtioFile = function (e) {
-        lowLag.msg("Error loading webkitAudio: " + e);
-    }
-
-    this.playSoundWebkitAudio = function (tag) {
-        lowLag.msg("playSoundWebkitAudio " + tag);
-        var buffer = lowLag.webkitAudioBuffers[tag];
-        if (buffer == undefined) { //possibly not loaded; put in a request to play onload
-            lowLag.webkitPendingRequest[tag] = true;
-            return;
-        }
-        var context = lowLag.webkitAudioContext;
-        if (this.useSuspension && this.suspended) {
-            this.resumePlaybackWebkitAudio(); // Resume playback
-        }
-        var source = context.createBufferSource(); // creates a sound source
-        source.buffer = buffer;                    // tell the source which sound to play
-        source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-        if (typeof(source.noteOn) == "function") {
-            source.noteOn(0);                          // play the source now, using noteOn
-        } else {
-            if (this.useSuspension) {
-                this.playingQueue.push(tag);
-                source.onended = function (e) {
-                    lowLag.hndlOnEndedWebkitAudio(tag, e);
-                }
-            }
-            source.start();				// play the source now, using start
-        }
-    }
-
-    this.hndlOnEndedWebkitAudio = function (tag, e) {
-        for (var i = 0; i < this.playingQueue.length; i++) {
-            if (this.playingQueue[i] == tag) {
-                this.playingQueue.splice(i, 1);
-                break;
-            }
-        }
-        if (!this.playingQueue.length) {
-            this.suspendPlaybackWebkitAudio();
-        }
-    }
-
-    this.resumePlaybackWebkitAudio = function () {
-        this.webkitAudioContext.resume();
-        this.suspended = false;
-    }
-
-    this.suspendPlaybackWebkitAudio = function () {
-        if (this.suspendTimeout) {
-            clearTimeout(this.suspendTimeout);
-        }
-        this.suspendTimeout = setTimeout(function () {
-            lowLag.webkitAudioContext.suspend();
-            lowLag.suspended = true;
-            lowLag.suspendTimeout = null;
-        }, this.suspendDelay);
-    }
-
-    this.audioTagID = 0;
-    this.audioTagNameToElement = {};
-
-    this.loadSoundAudioTag = function (urls, tag) {
-        var id = "lowLagElem_" + lowLag.audioTagID++;
-
-        var tag = lowLag.getTagFromURL(urls, tag);
-
-        var urls = lowLag.getURLArray(urls);
-
-        lowLag.audioTagNameToElement[tag] = id;
-
-        lowLag.msg('audioTag loading ' + urls + ' as tag ' + tag);
-        var audioElem = this.createElement("audio", {"id": id, "preload": "auto", "autobuffer": "autobuffer"})
-
-        for (var i = 0; i < urls.length; i++) {
-            var url = urls[i];
-            var type = "audio/" + lowLag.getExtension(url);
-            var sourceElem = this.createElement("source", {"src": lowLag.soundUrl + url, "type": type});
-            audioElem.appendChild(sourceElem);
-        }
-
-        document.body.appendChild(audioElem);
-    }
-
-    this.playSoundAudioTag = function (tag) {
-        lowLag.msg("playSoundAudioTag " + tag);
-
-        var modelId = lowLag.audioTagNameToElement[tag];
-        var cloneId = "lowLagCloneElem_" + lowLag.audioTagID++;
-
-        var modelElem = document.getElementById(modelId);
-        var cloneElem = modelElem.cloneNode(true);
-        cloneElem.setAttribute("id", cloneId);
-        this.divLowLag.appendChild(cloneElem);
-        lowLag.msg(tag);
-        if (lowLag.audioTagTimeToLive != -1) {
-            setTimeout(function () {
-                lowLag.safelyRemoveElement(cloneElem);
-            }, lowLag.audioTagTimeToLive);
-        }
-        cloneElem.play();
-
-    }
-
-    this.getExtension = function (url) {
-        return url.substring(url.lastIndexOf(".") + 1).toLowerCase();
-
-    }
-
-    this.msg = function (m) {
-        m = "-- lowLag " + m;
-        if (lowLag.debug == 'both' || lowLag.debug == 'console') {
-            console.log(m);
-        }
-        if (lowLag.divDebug) {
-            lowLag.divDebug.innerHTML += m + "\n";
-        }
-    }
-
 }
